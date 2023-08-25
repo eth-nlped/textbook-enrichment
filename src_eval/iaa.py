a347dd812dc3a166290875d641c377d9394b18ad
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-08-25 16:36:28 -0400

    add preliminary IAA

--- src_eval/iaa.py
+++ src_eval/iaa.py
@@ -6,6 +6,11 @@ import collections
 import numpy as np
 import re
 
+print(len([
+    f for f in glob.glob("data/prolific_pilot_1/*.jsonl")
+    if re.match(r".*s\d+.jsonl", f)
+]))
+
 data = [
     json.loads(line)
     for f in glob.glob("data/prolific_pilot_1/*.jsonl")
@@ -19,6 +24,7 @@ data_us = [
     for line in open(f, "r")
 ]
 
+
 def get_answer_score(answer):
     return {
         "no": 0,
@@ -30,39 +36,46 @@ def get_answer_score(answer):
         "undecided": 0.5,
     }[answer]
 
+
 print(len(data), "user lines")
 
 data_groupped = collections.defaultdict(list)
 for line in data:
     for img_key in line["responses"].keys():
-        data_groupped[(line["subsection"], line["mode"])].append(line["responses"][img_key])
+        data_groupped[
+            (line["subsection"], line["mode"], img_key)
+        ].append(line["responses"][img_key])
 
 
 exact_matches = []
+vague_matches = []
 for sig, values in data_groupped.items():
     if len(values) == 1:
         continue
-    
+
     for p1_i, p1 in enumerate(values):
-        for p2_i, p2 in enumerate(values[p1_i+1:]):
+        for p2_i, p2 in enumerate(values[p1_i + 1:]):
             for key in p1.keys():
                 if type(p1[key]) is dict:
                     continue
                 v1 = get_answer_score(p1[key])
                 v2 = get_answer_score(p2[key])
-                exact_matches.append(abs(v1-v2) != 1)
-            
+                vague_matches.append(abs(v1 - v2) == 0)
+                exact_matches.append(abs(v1 - v2) != 1)
 
-print(np.average(exact_matches))
+
+print(len(exact_matches), np.average(exact_matches), np.average(vague_matches))
 
 exact_matches = []
+vague_matches = []
 for line in data_us:
-    for p1 in line["responses"].values():
-        for p2 in data_groupped[(line["subsection"], line["mode"])]:
+    for img_key, p1 in line["responses"].items():
+        for p2 in data_groupped[(line["subsection"], line["mode"], img_key)]:
             for key in p1.keys():
                 if type(p1[key]) is dict:
                     continue
                 v1 = get_answer_score(p1[key])
                 v2 = get_answer_score(p2[key])
-                exact_matches.append(abs(v1-v2) != 1)
-print(np.average(exact_matches))
+                vague_matches.append(abs(v1 - v2) == 0)
+                exact_matches.append(abs(v1 - v2) != 1)
+print(len(exact_matches), np.average(exact_matches), np.average(vague_matches))
